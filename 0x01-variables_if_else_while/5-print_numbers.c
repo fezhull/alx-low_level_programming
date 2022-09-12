137d7c896639714045b52d8a95a2eed3aca69b07
fezhull <felizhielerfezull@gmail.com>
2022-09-12 12:07:05 -0700

    tast 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
@@ -8,14 +8,11 @@
 */
 int main(void)
 {
-
 int i;
 for (i = 0; i < 10; i++)
 {
 	printf("%i", i);
-
 }
 printf("\n");
 return (0);
-
 }
