a1cfe004c81aa234cbe2ef364ca226d0e16931cf
fezhull <felizhielerfezull@gmail.com>
2022-09-12 17:15:52 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-/**                                                                                                                              
-* main - Print the string in the put function                                                                                    
-*
-* Description: using the main function                                                                                           
-* this program prints "programming is like building a multilingual puzzle                                                        
-* Return: 0                                                                                                                      
-*/                                                                                                                               
-int main(void)                                                                                                                   
-{                                                                                                                                
-        char a;
+/**
+* main - Print the string in the put function
+* 
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0                                                                                                        */
+int main(void)               
+{
+       	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
-
-       	printf("Size of a char: %d byte(s)\n", sizeof(a));                                                                       
-        printf("Size of an int: %d byte(s)\n", sizeof(b));
+	
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d byte(s)\n", sizeof(e));
-	return (0);                                                                                                              
-}                              
+	return (0);
+}	
