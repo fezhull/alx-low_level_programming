dc6e860ea2743caeb675932967dd5beabb215650
fezhull <felizhielerfezull@gmail.com>
2022-09-19 16:25:59 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,38 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * rev_string -> reversing a string
- * @s: parameters
- */
+* rev_string - Reverses a string
+* @s: string to be reversed
+*
+* Return: nothing
+*/
+
 void rev_string(char *s)
 {
-	"sam" -> "mas"
-		char tmp;
-	int
+	int i, tmp, len = _strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+*
+* Return: the length of the given string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
