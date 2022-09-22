630749c7304a94d406d1c32086c37baad514186b
fezhull <felizhielerfezull@gmail.com>
2022-09-22 10:54:32 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,8 +5,8 @@
  * the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
- * 
- *  Return: returns value of dest
+ *
+ * *  Return: returns value of dest
 */
 
 char *_strcat(char *dest, char *src)
