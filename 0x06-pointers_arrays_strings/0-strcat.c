126825649c7d3ee5bbb09280d84428a859b29737
fezhull <felizhielerfezull@gmail.com>
2022-09-22 05:27:54 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,11 @@
  *@src: String to be concatenated upon
  * 
  *  Return: returns value of dest
- */
+*/
+
 char *_strcat(char *dest, char *src)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
