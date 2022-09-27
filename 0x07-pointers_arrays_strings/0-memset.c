fcb1cd5bacdb0cffaefb71f945bfe6654c24353b
fezhull <felizhielerfezull@gmail.com>
2022-09-27 06:06:56 -0700

    Tast 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @b: char to be used
  * @n: number of bytes to be used
  *
- * Return: pointer to the memory block
+ * Return pointer to the memory block
  */
 
 char *_memset(char *s, char b, unsigned int n)
