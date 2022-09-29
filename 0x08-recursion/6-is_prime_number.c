d8b122592f8c32d91053c92d36525b473753d08f
fezhull <felizhielerfezull@gmail.com>
2022-09-28 17:15:33 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,11 +16,11 @@ int is_prime_number(int n)
 }
 
 /**
- * is_prime - returns the 1 if n is prime 
+ * is_prime - returns the 1 if n is prime
  * @n: number to be checked
  * @start: number to start checking from
  *
- * Returnint start = n / 2;: 1 if n is prime, 0 otherwise
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime(int n, int start)
 {
